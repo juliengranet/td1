337706bafc22c1db0c048c8d826300bff73f62a9
juliengranet <ju.granet@laposte.net>
2020-09-18 18:00:42 +0200

    ajout des fonctions et de leur primitives

--- src/fonctions_test.py
+++ src/fonctions_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-
+import cmath as math
 # On définit ici des fonctions qui permettent de vérifier la précision
 # et l'ordre de convergence des formules de quadrature. Pour chaque
 # fonction, on définit donc sa primitive.
@@ -19,6 +19,19 @@ def primitive_monome(x):
     """primitive de la fonction x^k. Par défaut k=0."""
 
     return x**(degre+1)/(degre+1.)
-
+def abso(x):
+    return np.abs(x)
+def prim_abso(x):
+    return 0.5*x*np.abs(x)
+def trig(x):
+    return np.cos(x)
+def prim_trig(x):
+    return np.sin(x)
+def exp(x):
+    return np.exp(x)
+def pol(x):
+    return 1/(1+x^2)
+def prim_pol(x):
+    return math.atan(x)
 # Définir ici les autres fonctions utilisées pour des tests et leurs
 # primitives si elles sont connues
